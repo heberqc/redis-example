988fec4993eedf0a52fdfbaed7fe3ca489b3458a
Heber Quequejana <quequejanahu@globalhitss.com>
2019-06-21 15:59:36 -0500

    :tada: first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const redis_uri = 'redis://127.0.0.1:6379'
+
+const {
+  redisConnection,
+  redisGET,
+  redisSMEMBERS,
+  redisSADD,
+  redisSISMEMBER,
+} = require('./services/redis')
+
+redisConnection(redis_uri)
+
+redisGET('name')
+.then((name) => {
+  console.log('name:', name)
+})
+
+.then(() => redisSMEMBERS('agrupacion'))
+.then((agrupacion) => {
+  console.log('agrupacion:', agrupacion)
+})
+
+.then(() => redisSADD('prueba', 'uno', 'dos', 'tres', 'cuatro'))
+.then(agregaMiembros => {
+  console.log('agregaMiembros:', agregaMiembros)
+})
+
+.then(() => redisSMEMBERS('prueba'))
+.then(miembros => {
+  console.log('miembros:', miembros)
+})
+
+.then(() => redisSISMEMBER('prueba', 'dos'))
+.then(esMiembro => {
+  console.log('esMiembro:', esMiembro)
+})
---
+++ services/redis.js
@@ -0,0 +1,39 @@
+const redis = require('redis')
+
+let redisClient
+
+exports.redisConnection = uri => {
+  redisClient = redis.createClient(uri)
+} 
+
+// obtener un valor de tipo sring
+exports.redisGET = key => new Promise((resolve, reject) => {
+  redisClient.GET(key, (err, respuesta) => {
+    if (err) reject(err)
+    else resolve(respuesta)
+  })
+})
+
+// agregar agregar miembros a una agrupacion
+exports.redisSADD = (key, ...members) => new Promise((resolve, reject) => {
+  redisClient.SADD(key, members, (err, respuesta) => {
+    if (err) reject(err)
+    else resolve(respuesta)
+  })
+})
+
+// listas todos los miembros de una agrupacion
+exports.redisSMEMBERS = key => new Promise((resolve, reject) => {
+  redisClient.SMEMBERS(key, (err, respuesta) => {
+    if (err) reject(err)
+    else resolve(respuesta)
+  })
+})
+
+// verificar si un miembro es parte de una agrupacion
+exports.redisSISMEMBER = (key, member) => new Promise((resolve, reject) => {
+  redisClient.SISMEMBER(key, member, (err, respuesta) => {
+    if (err) reject(err)
+    else resolve(respuesta)
+  })
+})
